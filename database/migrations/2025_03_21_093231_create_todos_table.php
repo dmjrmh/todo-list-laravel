bd77314404add6f19119b923d10a33b2f625e4e9
Dame <gban1mj27@gmail.com>
2025-03-27 19:34:36 +0700

    added soft deletes

--- database/migrations/2025_03_21_093231_create_todos_table.php
+++ database/migrations/2025_03_21_093231_create_todos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('task');
             $table->boolean('is_done')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
